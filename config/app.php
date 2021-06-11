34cccc05bd8dbec9c9be527adee74e3cd7ded05b
ZR <zikriramdani.developer@gmail.com>
2021-06-11 11:50:00 +0700

    M

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://laravel1.netlify.app'),
+    'url' => env('APP_URL', 'https://herolaravelone.herokuapp.com/'),
     // 'url' => env('APP_URL', 'http://localhost'),
     
     'asset_url' => env('ASSET_URL', null),
